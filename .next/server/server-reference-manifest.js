e3c5b852f828dcca5eb488542d5c4ef3ba2e4250
Ram Office New PC <ramp00786@gmail.com>
2024-12-16 16:41:53 +0530

    first commit

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"0u2IXBORsBr8KF8km1BpklY6d1xH4Gsmnge44/OA7XQ=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
